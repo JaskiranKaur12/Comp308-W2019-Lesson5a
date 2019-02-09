bbf891fda27559177441ac678426e8844e610818
JaskiranKaur12 <kiranmujral@yahoo.com>
2019-02-09 10:28:39 -0500

    Mongoose Support

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+//IIFE-Immedialtely Invoked Function Expression
+(function(){
+  function Start()
+  {
+console.log(`%c App Started..`,
+"font-size: 20px; color:red; Font-weight: italic");
+
+  }
+ window.addEventListener("load",Start);
+})
